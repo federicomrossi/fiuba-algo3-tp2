d261958f0e44cc7938768c60dc37934a383f96da
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-15 22:08:11 +0000

    Esqueleto de las expeciones

---
+++ trunk/tp2-codigo-fuente/tp2/modelo/excepciones/JuegoNoIniciado.java
@@ -0,0 +1,6 @@
+package tp2.modelo.excepciones;
+
+public class JuegoNoIniciado extends Exception {
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ trunk/tp2-codigo-fuente/tp2/modelo/excepciones/VueloIniciado.java
@@ -0,0 +1,6 @@
+package tp2.modelo.excepciones;
+
+public class VueloIniciado extends Exception {
+
+	private static final long serialVersionUID = 1L;
+}
