bbd74b235e6d1eec298e892f860c0c7d0235c7d1
Gaston.martinez.90@live.com.ar <Gaston.martinez.90@live.com.ar@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-16 01:52:04 +0000

    Probando subir las pruebas. Todavia faltan imports y definir por que se reemplaza la clase punto de smalltalk y demas problemas.

---
+++ tp2-pruebas/tp2/modelo/test/ArmaDirigidaTest.java
@@ -0,0 +1,54 @@
+package tp2.modelo.test;
+import org.junit.*;
+
+public class ArmaDirigidaTest {
+	
+	private Escenario escenario;
+	private Arma arma;
+	private Flota flota;
+	private Proyectil proyectil1;
+	private Proyectil proyectil2;
+	private Proyectil proyectil3;
+	private Nave objetivo1; 
+	private Nave objetivo2;
+	
+	@before
+	public void Arrange(){
+		 escenario = new Escenario((0 @ 0) extent: (1 @ 1));
+		 arma = new ArmaDirigida ((10 @ 1),escenario,100,1,(4 @ 3),"arma");
+		 arma.CambiarModeloDeProyectilA(new Proyectil((0 @ 0),0.1,0,10);
+		 objetivo1 = new Nave((40 @ -234),1,escenario,1,100);
+		 objetivo2 = new Nave((22 @ 67),1,escenario,1,100);
+		 objetivo1.CambiarEquipoA("aliados");
+		 objetivo2.CambiarEquipoA("aliados");
+		 flota = new Flota(objetivo1);
+		 arma.CambiarFlotaObjetivoA(flota);
+	}
+	
+	@test
+	public void Disparar1(){		
+		//Disparamos el primer proyectil, y debería alcanzar a la únicaa nave de la flota
+		proyectil1 = arma.disparar();
+		proyectil1.MoverDurante(10000);
+		Assert.assertTrue((proyectil1.ObtenerPosicion().Distancia(objetivo1.ObtenerPosicion())) <= (1E-10));
+	}
+	
+	@test
+	public void Disparar2(){
+		//Disparamos un segundo proyectil, y debería alcanzar al segundo objetivo, porque destruimos el primero
+		flota.AgregarNave(objetivo2);
+		objetivo1.Destruir();
+		proyectil2 = arma.Disparar();
+		proyectil2.MoverDurante(10000);
+		Assert.assertTrue((proyectil2.ObtenerPosicion().Distancia(objetivo2.ObtenerPosicion())) <= (1E-10));
+	}
+	
+	@test
+	public void Disparar3(){
+		//Disparamos un tercer proyectil con todos los objetivos destruídos, y tiene que seguir la dirección de vuelo indicada
+		objetivo1.Destruir();
+		objetivo2.Destruir();
+		proyectil2 = arma.Disparar();
+		proyectil2.MoverDurante(10);
+		Assert.assertTrue((proyectil2.ObtenerPosicion().Distancia((810 @ 601))) <= (1e-10));
+	}
