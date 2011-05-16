12f2dc0dfb46d1ec5c09d66ffda6b0e1ca5ea4a5
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-16 23:56:41 +0000

    Parte de ObjetoEspacial

--- trunk/tp2-codigo-fuente/tp2/modelo/Movil.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/Movil.java
@@ -1,9 +1,16 @@
 package tp2.modelo;
 
+import tp2.auxiliares.Point;
+
 public abstract class Movil extends ObjetoEspacial {
 	
-	public Movil(){
-		
+	private double velocidad;
+
+	public Movil(Point posicion, double tamano, Escenario escenario, double velocidad) {
+		super(posicion, tamano, escenario);
+		// Inicializa el móvil con la posición, tamaño, escenario y velocidad
+		// recibidos. Lo agrega al escenario.
+		this.velocidad = velocidad;
 	}
-	
+
 }
