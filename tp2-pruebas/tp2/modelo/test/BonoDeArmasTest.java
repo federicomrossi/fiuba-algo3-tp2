3e03abe9d3c73782fbe19cf486548528dae4a743
Gaston.martinez.90@live.com.ar <Gaston.martinez.90@live.com.ar@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-17 03:02:51 +0000

    Agregadas pruebas de Bonos

---
+++ tp2-pruebas/tp2/modelo/test/BonoDeArmasTest.java
@@ -0,0 +1,35 @@
+package tp2.modelo.test;
+
+import java.awt.Rectangle;
+
+import org.junit.*;
+
+import tp2.modelo.*;
+import tp2.auxiliares.*;
+
+public class BonoDeArmasTest {
+	
+	private Bono bono;
+	private Nave nave;
+	private Nave naveReceptora;
+	private Arma arma;
+	private Escenario escenario;	
+	
+	@Before
+	public void Arrange(){
+		escenario = new Escenario(new Rectangle());
+		nave = new NaveEnemiga(new Point(243,71),1,escenario,1,100);
+		naveReceptora = new NaveMilitarControlada((2,271),1,escenario,1,100);
+		arma = new Arma(new Point(5,5),escenario,100,1,(4,3),"arma");
+		nave.AgregarArma(arma);
+		bono = new BonoDeArmas(1);
+		nave.CambiarBonoA(bono);
+	}
+	
+	@Test
+	public void TestEntregarBonoA(){
+		//Le damos el bono a la nave receptora, y vemos si efectivamente recibió el arma
+		bono.EntregarBonoA(naveReceptora);
+		Assert.assertTrue((naveReceptora.ObtenerArmas().size()) = 1);
+	}
+}
---
+++ tp2-pruebas/tp2/modelo/test/BonoDeEnergiaTest.java
@@ -0,0 +1,34 @@
+package tp2.modelo.test;
+
+import java.awt.Rectangle;
+
+import org.junit.*;
+
+import tp2.modelo.*;
+import tp2.auxiliares.*;
+
+public class BonoDeEnergiaTest {	
+	
+	private Bono bono;
+	private Nave nave;
+	private Nave naveReceptora;
+	private Escenario escenario;	
+
+	@Before
+	public void Arrange(){
+		escenario = new Escenario(new Rectangle());
+		nave = new NaveEnemiga(new Point(243,71),1,escenario,1,50);
+		naveReceptora = new NaveMilitarControlada((2,271),1,escenario,1,200);
+		naveReceptora.RecibirDanio(100);
+		bono = new BonoDeEnergia(1);
+		nave.CambiarBonoA(bono);
+	}
+	
+	@Test
+	public void TestEntregarBonoA(){
+		//Le damos el bono a la nave receptora, y vemos si efectivamente recibió la energía
+		bono.EntregarBonoA(naveReceptora);
+		Assert.assertTrue(naveReceptora.ObtenerEnergia() = 150);
+	}
+
+}
