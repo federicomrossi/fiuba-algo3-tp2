034b945fa2bc6065ecfb9f97297b8cb74d7cfb99
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-18 04:03:12 +0000

    Clase ChoqueDeObjetoEspacial. Falta codear el constructor y un método que lanza excepción.

---
+++ trunk/tp2-codigo-fuente/tp2/modelo/ChoqueDeObjetoEspacial.java
@@ -0,0 +1,74 @@
+package tp2.modelo;
+
+
+// Esta clase define el comportamiento receptor de un objeto espacial frente a los 
+// demás. Cada método indica cómo se ve afectado el objeto espacial al chocar con
+// un tipo particular de objeto.
+// Considerando al objeto espacial como el objeto desde el cual todos heredan, 
+// los demás comportamientos deberían heredar de éste, entendiendo las siguiente 
+// reglas al definir comportamiento:
+//	- Una nueva clase de comportamiento indica que el tipo de objeto se ve afectado 
+//    de forma diferente a los demás.
+//	- Un nuevo método indica que cierto tipo de objeto afecta de diferente manera 
+//    a ciertos tipos de objetos.
+public class ChoqueDeObjetoEspacial {
+
+	private ObjetoEspacial objeto;
+	
+	public ChoqueDeObjetoEspacial(ObjetoEspacial unObjetoEspacial) {
+		/////////////////////////////////////////////////////////
+		// TERMINAR
+		/////////////////////////////////////////////////////////
+	}
+	
+	public void cambiarObjetoA(ObjetoEspacial nuevoObjeto) {
+		objeto = nuevoObjeto;
+	}
+	
+	public ObjetoEspacial obtenerObjeto() {
+		return this.objeto;
+	}
+	
+	// Por defecto, a un objeto espacial no le pasa nada cuando choca con un bono.
+	public void sufrirChoqueDeBono(Bono unBono)
+	{
+		this.verificarInicializacion();
+	}
+	
+	// Por defecto, a un objeto espacial no le pasa nada cuando choca con una nave.
+	public void sufrirChoqueDeNave(Nave unaNave)
+	{
+		this.verificarInicializacion();
+	}
+	
+	// Por defecto, a un objeto espacial no le pasa nada cuando choca con una nave civil.
+	public void sufrirChoqueDeNaveCivil(NaveCivil unaNaveCivil)
+	{
+		this.verificarInicializacion();
+	}
+	
+	// Por defecto, a un objeto espacial no le pasa nada cuando choca con una nave militar.
+	public void sufrirChoqueDeNaveMilitar(NaveMilitar unaNaveMilitar)
+	{
+		this.verificarInicializacion();
+	}
+	
+	// Por defecto, a un objeto espacial no le pasa nada cuando choca con otro objeto espacial.
+	public void sufrirChoqueDeObjetoEspacial(ObjetoEspacial unObjetoEspacial)
+	{
+		this.verificarInicializacion();
+	}
+	
+	// Por defecto, a un objeto espacial no le pasa nada cuando choca con un proyectil.
+	public void sufrirChoqueDeProyectil(Proyectil unProyectil)
+	{
+		this.verificarInicializacion();
+	}
+	
+	private void verificarInicializacion(){
+		/////////////////////////////////////////////////////////
+		// TERMINAR
+		/////////////////////////////////////////////////////////	
+	}
+	
+}
