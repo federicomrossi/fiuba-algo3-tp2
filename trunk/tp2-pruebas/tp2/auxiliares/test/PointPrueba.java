54e5e612488503271629478e90b0455b4e6c1e49
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-18 02:35:26 +0000

    Refactorizando nombres de métodos con las convenciones a seguir.

--- trunk/tp2-pruebas/tp2/auxiliares/test/PointPrueba.java
+++ trunk/tp2-pruebas/tp2/auxiliares/test/PointPrueba.java
@@ -9,22 +9,22 @@ import junit.framework.TestCase;
 public class PointPrueba extends TestCase{
 	
 	@Test
-	public void testSumar(){
+	public void testSumarCon(){
 		Point punto = new Point(3, 4);
 		Point desplazamiento = new Point(5, 6);
 		// Probamos de sumar los dos puntos
-		punto = punto.sumar(desplazamiento);
+		punto = punto.sumarCon(desplazamiento);
 		double error = punto.distance(new Point(8, 10));
 		assertEquals(0, error, 1e-10);
 	}
 	
 	@Test
-	public void testRotar(){
+	public void testRotarEn(){
 		Point punto = new Point(5, 6);
 		Point centro = new Point(5, 7);
 		double angulo = Math.PI / 4;
 		// Rotamos el punto respecto al centro indicado
-		punto.rotar(angulo, centro);
+		punto.rotarEn(angulo, centro);
 		Point resultado = new Point(5 + Math.sqrt(2) / 2, 7 - Math.sqrt(2)/2);
 		double error = punto.distance(resultado);
 		assertEquals(0, error, 1e-10);
