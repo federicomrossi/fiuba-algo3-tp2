0a585abafba85d453ca44e88d2a1eb942650ec7a
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-18 13:23:04 +0000

    Hecho el esqueleto de la clase ChoqueDeNave.

--- tp2-codigo-fuente/tp2/modelo/ObjetoVolador.java
+++ tp2-codigo-fuente/tp2/modelo/ObjetoVolador.java
@@ -4,9 +4,8 @@ import tp2.auxiliares.Point;
 
 public abstract class ObjetoVolador extends Movil {
 
-	public ObjetoVolador(Point posicion, double tamano, Escenario escenario,
-			double velocidad) {
-		super(posicion, tamano, escenario, velocidad);
+	public ObjetoVolador(Point posicion, double tamanio, Escenario escenario, double velocidad) {
+		super(posicion, tamanio, escenario, velocidad);
 		// Inicializa el objeto volador sin un vuelo asignado, con la posición,
 		// tamaño, escenario y velocidad recibidos. Lo agrega al escenario.
 	}
