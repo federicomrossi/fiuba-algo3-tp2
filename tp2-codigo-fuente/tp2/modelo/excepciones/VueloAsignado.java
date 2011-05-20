3b8bca96eb98ac4f25aa2f4dbec7c759210d2fa3
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-20 23:45:03 +0000

    Clase Vuelo + cambiar herencia de excepciones a RuntimeException + constructores de las excepciones.

--- tp2-codigo-fuente/tp2/modelo/excepciones/VueloAsignado.java
+++ tp2-codigo-fuente/tp2/modelo/excepciones/VueloAsignado.java
@@ -1,6 +1,26 @@
 package tp2.modelo.excepciones;
 
-public class VueloAsignado extends Exception {
+public class VueloAsignado extends RuntimeException {
+
+	public VueloAsignado() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VueloAsignado(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VueloAsignado(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VueloAsignado(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 
 	private static final long serialVersionUID = 1L;
 }
