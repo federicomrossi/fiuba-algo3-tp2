94cb2f2acb8eb8dd27f300dd2b389e7bc0c1b4cf
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-20 23:45:03 +0000

    Clase Vuelo + cambiar herencia de excepciones a RuntimeException + constructores de las excepciones.

--- trunk/tp2-codigo-fuente/tp2/modelo/Vuelo.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/Vuelo.java
@@ -1,12 +1,61 @@
 package tp2.modelo;
 
-public abstract class Vuelo {
-	
-	ObjetoVolador objetoVolador;
+import java.util.EmptyStackException;
 
+import tp2.auxiliares.Point;
+import tp2.modelo.excepciones.VueloIniciado;
+
+public abstract class Vuelo {
+
+	private ObjetoVolador objetoVolador;
+	private boolean iniciado;
+	private Point origen;
+	private double trayectoriaDeVuelo;
+
+	// Inicializa un vuelo del objeto volador recibido.
 	public Vuelo(ObjetoVolador objetoVolador) {
 		super();
 		this.objetoVolador = objetoVolador;
+		this.iniciado = false;
+		this.origen = null;
+		this.trayectoriaDeVuelo = 0.0;
 	}
 
+	public ObjetoVolador getObjetoVolador() {
+		return objetoVolador;
+	}
+
+	public boolean estaIniciado() {
+		return iniciado;
+	}
+
+	public Point getOrigen() {
+		return origen;
+	}
+
+	public double getTrayectoriaDeVuelo() {
+		return trayectoriaDeVuelo;
+	}
+
+	// Inicia el vuelo con su origen en el lugar actual donde está posicionado
+	// el objeto. Si el vuelo ya fue iniciado, se levanta una excepción.
+	public void iniciar(){
+		if (this.estaIniciado()) {
+			throw new VueloIniciado("El vuelo ya fue iniciado anteriormente.");
+		}
+		this.origen = objetoVolador.getPosicion();
+		this.iniciado = true;
+	}
+
+	// Avanza el vuelo según el tiempo recibido, calcula el desplazamiento de la
+	// nave en dicho intervalo y devuelve el mismo. En cada reimplementación, si
+	// el vuelo no ha iniciado, debe levantarse una excepción del tipo
+	// VueloNoIniciado.
+	public abstract void avanzarDurante(double tiempo);
+
+	// Aumenta el valor de la trayectoria de vuelo de acuerdo al valor, que debe
+	// ser mayor a cero, recibido.
+	protected void aumentarTrayectoriaEn(double longitud){
+		this.trayectoriaDeVuelo = this.trayectoriaDeVuelo + longitud;
+	}
 }
