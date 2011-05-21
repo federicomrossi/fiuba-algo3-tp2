7a136800a73f54228ce30a5685921fee677db13b
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-21 16:02:27 +0000

    Modificacion parcial de ArmaDirigidaTest.java

--- tp2-pruebas/tp2/modelo/test/ArmaDirigidaTest.java
+++ tp2-pruebas/tp2/modelo/test/ArmaDirigidaTest.java
@@ -23,13 +23,13 @@ public class ArmaDirigidaTest{
 	public void arrange(){
 		 escenario = new Escenario(new Rectangle(new Dimension(1,1)));
 		 arma = new ArmaDirigida (new Point(10,1),escenario,100,1,new Point(4,3),"arma");
-		 arma.cambiarModeloDeProyectilA(new Proyectil(new Point(0,0),0.1,0,10);
+		 arma.setModeloDeProyectil(new Proyectil(new Point(0,0),0.1,0,10);
 		 objetivo1 = new Nave(new Point(40,-234),1,escenario,1,100);
 		 objetivo2 = new Nave(new Point(22,67),1,escenario,1,100);
-		 objetivo1.cambiarEquipoA("aliados");
-		 objetivo2.cambiarEquipoA("aliados");
+		 objetivo1.setEquipo("aliados");
+		 objetivo2.setEquipo("aliados");
 		 flota = new Flota(objetivo1);
-		 arma.cambiarFlotaObjetivoA(flota);
+		 arma.setFlotaObjetivo(flota);
 	}
 	
 	@Test
@@ -37,7 +37,7 @@ public class ArmaDirigidaTest{
 		//Disparamos el primer proyectil, y debería alcanzar a la únicaa nave de la flota
 		proyectil1 = arma.disparar();
 		proyectil1.moverDurante(10000);
-		Assert.assertTrue ((proyectil1.obtenerPosicion().distancia(objetivo1.obtenerPosicion())) <= (1E-10));
+		Assert.assertTrue ((proyectil1.getPosicion().distancia(objetivo1.getPosicion())) <= (1E-10));
 	}
 	
 	@Test
@@ -47,7 +47,7 @@ public class ArmaDirigidaTest{
 		objetivo1.destruir();
 		proyectil2 = arma.disparar();
 		proyectil2.moverDurante(10000);
-		Assert.assertTrue((proyectil2.obtenerPosicion().distancia(objetivo2.obtenerPosicion())) <= (1E-10));
+		Assert.assertTrue((proyectil2.getPosicion().distancia(objetivo2.getPosicion())) <= (1E-10));
 	}
 	
 	@Test
