2cd90cb6b3ddb8d1975725f3d362de81ee8da554
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-21 13:49:33 +0000

    Actualizo ObjetoEspacial

--- trunk/tp2-codigo-fuente/tp2/modelo/ObjetoEspacial.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/ObjetoEspacial.java
@@ -8,10 +8,14 @@ public abstract class ObjetoEspacial {
 	private double tamanio;
 	private Escenario escenario;
 	private boolean destruido;
+	private ChoqueDeObjetoEspacial comportamientoAlChocar;
 
+
+	// Constructor
 	// Inicializa el objeto con la posición, tamaño y escenario recibidos.
 	// Lo agrega al escenario.
 	public ObjetoEspacial(Point posicion, double tamanio, Escenario escenario) {
+
 		this.posicion = posicion;
 		this.tamanio = tamanio;
 		this.escenario = escenario;
@@ -19,25 +23,12 @@ public abstract class ObjetoEspacial {
 		// Falta agregarlo al escenario
 	}
 
-	public Point getPosicion() {
-		return posicion;
-	}
-
-	public double getTamanio() {
-		return tamanio;
-	}
-
-	public Escenario getEscenario() {
-		return escenario;
-	}
-
-	public void setPosicion(Point posicion) {
-		this.posicion = posicion;
-	}
-
+	// Devuelve true si el objeto recibido está superpuesto con el receptor
+	// del mensaje. Sino false.
 	// Devuelve true si el objeto recibido está superpuesto con el receptor
 	// del mensaje. Sino false.
 	public boolean estaSuperpuestoCon(ObjetoEspacial objetoEspacial) {
+
 		double distanciaMaxima, distancia;
 		distanciaMaxima = this.getTamanio() + objetoEspacial.getTamanio();
 		distancia = this.getPosicion().distance(objetoEspacial.getPosicion());
@@ -49,8 +40,65 @@ public abstract class ObjetoEspacial {
 	// debería permitir actuar a objetos que no tengan un escenario, y además,
 	// si alguno está destruido, debería desaparecer de su escenario y no
 	// realizar nada más.
-	public abstract void actuarDurante(double tiempo);
+	public abstract void actuarDurante(double unTiempo);
 
+	// Hace chocar al objeto recibido con el receptor del mensaje. Ambos sufren el 
+	// efecto del mismo según cómo esté definido su comportamiento ante el mismo.
+	public void chocarCon(ObjetoEspacial unObjetoEspacial) {
+		
+	}
+		
+	// Este método tiene carácter de 'protegido de clase' (sólo deben llamarlo 
+	// objetos de esta dentro de esta jerarquía). El objeto que recibe el mensaje 
+	// responde el choque y le hace sufrir al objeto recibido sus efectos, de 
+	// acuerdo al comportamiento frente al choque que este último tenga.
+	// El receptor le pide al objeto recibido que le diga cómo tiene que ser 
+	// afectado al chocar con los demás objetos existentes. Luego es el receptor el 
+	// que selecciona de entre todo el comportamiento el correspondiente a él mismo.
+	public void responderChoqueDe(ObjetoEspacial unObjetoEspacial) {
+		
+	}
+		
+	// Hace que el objeto receptor del mensaje sea afectado por el bono recibido.
+	public void sufrirChoqueDeBono(Bono unBono) {
+		
+	}
+	
+	// Hace que el objeto receptor del mensaje sea afectado por la nave recibida.
+	public void sufrirChoqueDeNave(Nave unaNave) {
+		
+	}
+	
+	// Hace que el objeto receptor del mensaje sea afectado por  por la nave civil 
+	// recibida.
+	public void sufrirChoqueDeNaveCivil(NaveCivil unaNaveCivil) {
+		
+	}
+	
+	// Hace que el objeto receptor del mensaje sea afectado por la nave militar 
+	// recibida.
+	public void sufrirChoqueDeNaveMilitar(NaveMilitar unaNaveMilitar) {
+		
+	}
+	
+	// Hace que el objeto receptor del mensaje sea afectado por el objeto espacial 
+	// recibido.
+	public void sufrirChoqueDeObjetoEspacial(ObjetoEspacial unObjetoEspacial) {
+		
+	}
+	
+	// Hace que el objeto receptor del mensaje sea afectado por el proyectil 
+	// recibido.
+	public void sufrirChoqueDeProyectil(Proyectil unProyectil) {
+		
+	}
+	
+	// El objeto se borra del escenario. Si no pertenece a ninguno, se levanta una 
+	// excepción.
+	public void desaparecerDelEscenario() {
+		
+	}
+	
 	// Destruye al objeto y le impide realizar acciones en el futuro.
 	public void destruir() {
 		destruido = true;
@@ -59,5 +107,32 @@ public abstract class ObjetoEspacial {
 	public boolean estaDestruido() {
 		return destruido;
 	}
+		
+	public Point getPosicion() {
+		return posicion;
+	}
+	
+	public void setPosicion(Point posicion) {
+		this.posicion = posicion;
+	}
 
+	public double getTamanio() {
+		return tamanio;
+	}
+	
+	public void setTamanio(double nuevoTamanio) {
+		
+	}
+
+	public Escenario getEscenario() {
+		return escenario;
+	}
+	
+	public void setEscenario(Escenario nuevoEscenario) {
+		
+	}
+	
+	public ChoqueDeObjetoEspacial getComportamiento() {
+		return comportamientoAlChocar;
+	}
 }
