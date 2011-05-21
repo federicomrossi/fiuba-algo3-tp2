2144e9812cff8372b757989de5794107c08e5df4
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-21 16:27:21 +0000

    Fixed: ArmaDirigidaTest.java

--- trunk/tp2-pruebas/tp2/modelo/test/ArmaDirigidaTest.java
+++ trunk/tp2-pruebas/tp2/modelo/test/ArmaDirigidaTest.java
@@ -11,21 +11,20 @@ import tp2.auxiliares.*;
 public class ArmaDirigidaTest{
 	
 	private Escenario escenario;
-	private Arma arma;
+	private ArmaDirigida arma;
 	private Flota flota;
 	private Proyectil proyectil1;
 	private Proyectil proyectil2;
-	private Proyectil proyectil3;
-	private Nave objetivo1; 
-	private Nave objetivo2;
+	private NaveMilitar objetivo1; 
+	private NaveMilitar objetivo2;
 	
 	@Before 
 	public void arrange(){
 		 escenario = new Escenario(new Rectangle(new Dimension(1,1)));
 		 arma = new ArmaDirigida (new Point(10,1),escenario,100,1,new Point(4,3),"arma");
-		 arma.setModeloDeProyectil(new Proyectil(new Point(0,0),0.1,0,10);
-		 objetivo1 = new Nave(new Point(40,-234),1,escenario,1,100);
-		 objetivo2 = new Nave(new Point(22,67),1,escenario,1,100);
+		 arma.setModeloDeProyectil(new Proyectil(new Point(0,0),0.1,0,10));
+		 objetivo1 = new NaveMilitar(new Point(40,-234),1,escenario,1,100);
+		 objetivo2 = new NaveMilitar(new Point(22,67),1,escenario,1,100);
 		 objetivo1.setEquipo("aliados");
 		 objetivo2.setEquipo("aliados");
 		 flota = new Flota(objetivo1);
@@ -37,7 +36,7 @@ public class ArmaDirigidaTest{
 		//Disparamos el primer proyectil, y debería alcanzar a la únicaa nave de la flota
 		proyectil1 = arma.disparar();
 		proyectil1.moverDurante(10000);
-		Assert.assertTrue ((proyectil1.getPosicion().distancia(objetivo1.getPosicion())) <= (1E-10));
+		Assert.assertTrue ((proyectil1.getPosicion().distance(objetivo1.getPosicion())) <= (1E-10));
 	}
 	
 	@Test
@@ -47,15 +46,16 @@ public class ArmaDirigidaTest{
 		objetivo1.destruir();
 		proyectil2 = arma.disparar();
 		proyectil2.moverDurante(10000);
-		Assert.assertTrue((proyectil2.getPosicion().distancia(objetivo2.getPosicion())) <= (1E-10));
+		Assert.assertTrue((proyectil2.getPosicion().distance(objetivo2.getPosicion())) <= (1E-10));
 	}
 	
 	@Test
-	public void TestDisparar3(){
+	public void TestDisparar3() {
 		//Disparamos un tercer proyectil con todos los objetivos destruídos, y tiene que seguir la dirección de vuelo indicada
 		objetivo1.destruir();
 		objetivo2.destruir();
 		proyectil2 = arma.disparar();
 		proyectil2.moverDurante(10);
-		Assert.assertTrue((proyectil2.obtenerPosicion().distancia(new Point(810,601))) <= (1E-10));
+		Assert.assertTrue((proyectil2.getPosicion().distance(new Point(810,601))) <= (1E-10));
 	}
+}
