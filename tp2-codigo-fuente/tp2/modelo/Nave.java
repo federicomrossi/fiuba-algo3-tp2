be0d763b1a8df17e70721971d915ba2344d05337
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-21 13:52:08 +0000

    Algunas modificaciones poco significativas + clase Nave + clase NaveCivil + clase NaveMilitar

--- tp2-codigo-fuente/tp2/modelo/Nave.java
+++ tp2-codigo-fuente/tp2/modelo/Nave.java
@@ -4,13 +4,21 @@ import tp2.auxiliares.Point;
 
 public class Nave extends ObjetoVolador {
 
-	private double energia;
+	protected double energia;
 
+<<<<<<< .mine
+	// Inicializa la nave sin un vuelo asignado, con la posición, tamaño,
+	// escenario, velocidad y energía recibidos. La agrega al escenario.
+	public Nave(Point posicion, double tamanio, Escenario escenario, double velocidad, double energia) {
+		super(posicion, tamanio, escenario, velocidad);
+		
+=======
 	// Inicializa la nave sin un vuelo asignado, con la posición, tamaño,
 	// escenario, velocidad y energía recibidos. La agrega al escenario.
 	public Nave(Point posicion, double tamano, Escenario escenario,
 			double velocidad, double energia) {
 		super(posicion, tamano, escenario, velocidad);
+>>>>>>> .r49
 		this.energia = energia;
 	}
 
@@ -18,7 +26,15 @@ public class Nave extends ObjetoVolador {
 	// tener un vuelo y un escenario especificados, o de lo contrario se
 	// levanta una excepción.
 	@Override
+<<<<<<< .mine
+	// Ordena a la nave actuar durante el tiempo específicado. La nave debe
+	// tener un vuelo y un escenario especificados, o de lo contrario se
+	// levanta una excepción.
+	public void actuarDurante(double unTiempo) {
+		
+=======
 	public void actuarDurante(double tiempo) {
+>>>>>>> .r49
 	}
 
 }
