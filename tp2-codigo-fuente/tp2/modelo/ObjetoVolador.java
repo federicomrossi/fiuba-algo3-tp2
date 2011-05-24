47302cf82dad9b1e7b7414814ac2a5061fedd9f8
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-24 01:37:41 +0000

    Clase ObjetoVolador terminada.

--- tp2-codigo-fuente/tp2/modelo/ObjetoVolador.java
+++ tp2-codigo-fuente/tp2/modelo/ObjetoVolador.java
@@ -1,6 +1,7 @@
 package tp2.modelo;
 
 import tp2.auxiliares.Point;
+import tp2.modelo.excepciones.*;
 
 public abstract class ObjetoVolador extends Movil {
 
@@ -11,14 +12,25 @@ public abstract class ObjetoVolador extends Movil {
 	// tamaño, escenario y velocidad recibidos. Lo agrega al escenario.
 	public ObjetoVolador(Point posicion, double tamanio, Escenario escenario, double velocidad) {
 		super(posicion, tamanio, escenario, velocidad);
+		this.vuelo = null;
 	}
 
 	@Override
 	// Mueve al objeto volador de acuerdo al tiempo recibido y el vuelo que
 	// tenga. Si no tiene un vuelo definido o un escenario asignado, se
 	// levanta una excepción.
-	public void moverDurante(double unTiempo) {
-	
+	public void moverDurante(double unTiempo) throws ComposicionIncompleta {
+				
+		if(vuelo == null)
+			throw new ComposicionIncompleta("No se ha definido un vuelo para el objeto.");
+		
+		if(this.getEscenario() == null)
+			throw new ComposicionIncompleta("No se ha asignado un escenario al objeto.");
+		
+		if(! this.vuelo.estaIniciado()) 
+			this.vuelo.iniciar();
+		
+		this.desplazarEn(this.vuelo.avanzarDurante(unTiempo));
 	}
 	
 	public Vuelo getVuelo() {
@@ -28,6 +40,6 @@ public abstract class ObjetoVolador extends Movil {
 	// Asigna un nuevo Vuelo al objeto volador. De ahora en más, el objeto va a 
 	// volar de acuerdo a dicho vuelo en cada acción que realice.
 	public void setVuelo(Vuelo nuevoVuelo) {
-		
+		this.vuelo = nuevoVuelo;
 	}
 }
