631828f259f3fcdc48ca2342492ca6e42e2923f7
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-24 01:26:45 +0000

    Clase ObjetoEspacial terminada.

--- tp2-codigo-fuente/tp2/modelo/ObjetoEspacial.java
+++ tp2-codigo-fuente/tp2/modelo/ObjetoEspacial.java
@@ -1,6 +1,7 @@
 package tp2.modelo;
 
 import tp2.auxiliares.Point;
+import tp2.modelo.excepciones.*;
 
 public abstract class ObjetoEspacial {
 
@@ -20,7 +21,8 @@ public abstract class ObjetoEspacial {
 		this.tamanio = tamanio;
 		this.escenario = escenario;
 		this.destruido = false;
-		// Falta agregarlo al escenario
+		this.comportamientoAlChocar = new ChoqueDeObjetoEspacial(this);
+		this.escenario.agregarObjeto(this);
 	}
 
 	// Devuelve true si el objeto recibido está superpuesto con el receptor
@@ -46,6 +48,8 @@ public abstract class ObjetoEspacial {
 	// efecto del mismo según cómo esté definido su comportamiento ante el mismo.
 	public void chocarCon(ObjetoEspacial unObjetoEspacial) {
 		
+		this.responderChoqueDe(unObjetoEspacial);
+		unObjetoEspacial.responderChoqueDe(this);
 	}
 		
 	// Este método tiene carácter de 'protegido de clase' (sólo deben llamarlo 
@@ -56,56 +60,61 @@ public abstract class ObjetoEspacial {
 	// afectado al chocar con los demás objetos existentes. Luego es el receptor el 
 	// que selecciona de entre todo el comportamiento el correspondiente a él mismo.
 	public void responderChoqueDe(ObjetoEspacial unObjetoEspacial) {
-		
+		unObjetoEspacial.sufrirChoqueDeObjetoEspacial(this);
 	}
 		
 	// Hace que el objeto receptor del mensaje sea afectado por el bono recibido.
 	public void sufrirChoqueDeBono(Bono unBono) {
-		
+		this.comportamientoAlChocar.sufrirChoqueDeBono(unBono);
 	}
 	
 	// Hace que el objeto receptor del mensaje sea afectado por la nave recibida.
 	public void sufrirChoqueDeNave(Nave unaNave) {
-		
+		this.comportamientoAlChocar.sufrirChoqueDeNave(unaNave);
 	}
 	
 	// Hace que el objeto receptor del mensaje sea afectado por  por la nave civil 
 	// recibida.
 	public void sufrirChoqueDeNaveCivil(NaveCivil unaNaveCivil) {
-		
+		this.comportamientoAlChocar.sufrirChoqueDeNaveCivil(unaNaveCivil);
 	}
 	
 	// Hace que el objeto receptor del mensaje sea afectado por la nave militar 
 	// recibida.
 	public void sufrirChoqueDeNaveMilitar(NaveMilitar unaNaveMilitar) {
-		
+		this.comportamientoAlChocar.sufrirChoqueDeNaveMilitar(unaNaveMilitar);
 	}
 	
 	// Hace que el objeto receptor del mensaje sea afectado por el objeto espacial 
 	// recibido.
 	public void sufrirChoqueDeObjetoEspacial(ObjetoEspacial unObjetoEspacial) {
-		
+		this.comportamientoAlChocar.sufrirChoqueDeObjetoEspacial(unObjetoEspacial);
 	}
 	
 	// Hace que el objeto receptor del mensaje sea afectado por el proyectil 
 	// recibido.
 	public void sufrirChoqueDeProyectil(Proyectil unProyectil) {
-		
+		this.comportamientoAlChocar.sufrirChoqueDeProyectil(unProyectil);
 	}
 	
 	// El objeto se borra del escenario. Si no pertenece a ninguno, se levanta una 
 	// excepción.
-	public void desaparecerDelEscenario() {
+	public void desaparecerDelEscenario() throws ComposicionIncompleta {
 		
+		if(this.escenario == null) {
+			throw new ComposicionIncompleta("El objeto no está en ningún escenario.");
+		}
+		
+		this.escenario.borrarObjeto(this);
 	}
 	
 	// Destruye al objeto y le impide realizar acciones en el futuro.
 	public void destruir() {
-		destruido = true;
+		this.destruido = true;
 	}
 
 	public boolean estaDestruido() {
-		return destruido;
+		return this.destruido;
 	}
 		
 	public Point getPosicion() {
@@ -117,22 +126,25 @@ public abstract class ObjetoEspacial {
 	}
 
 	public double getTamanio() {
-		return tamanio;
+		return this.tamanio;
 	}
 	
 	public void setTamanio(double nuevoTamanio) {
-		
+		this.tamanio = nuevoTamanio;
 	}
 
 	public Escenario getEscenario() {
-		return escenario;
+		return this.escenario;
 	}
 	
+	// Cambia el escenario en donde se encuentra el objeto y agrega al mismo 
+	// en el nuevo.
 	public void setEscenario(Escenario nuevoEscenario) {
-		
+		this.escenario = nuevoEscenario;
+		this.escenario.agregarObjeto(this);
 	}
 	
 	public ChoqueDeObjetoEspacial getComportamiento() {
-		return comportamientoAlChocar;
+		return this.comportamientoAlChocar;
 	}
 }
