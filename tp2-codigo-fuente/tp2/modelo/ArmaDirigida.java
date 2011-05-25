e553eab09e257a6b5bbb04b9e7f0aa6adea594ed
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-25 21:28:54 +0000

    Clases ArmaDirigida y ArmaLimitada

--- tp2-codigo-fuente/tp2/modelo/ArmaDirigida.java
+++ tp2-codigo-fuente/tp2/modelo/ArmaDirigida.java
@@ -1,5 +1,7 @@
 package tp2.modelo;
 
+import java.util.Iterator;
+
 import tp2.auxiliares.Point;
 
 // Es un arma limitada, que puede configurarse para disparar contra una flota 
@@ -16,6 +18,8 @@ public class ArmaDirigida extends ArmaLimitada {
 	// carga inicial es cero y no hay objetivo asignado
 	public ArmaDirigida(Point posicion, Escenario escenario, int velocidad, double frecuencia, Point direccion, String id) {
 		super(posicion, escenario, velocidad, frecuencia, direccion, id);
+		
+		flotaObjetivo = null;
 	}
 	
 	public Flota getFlotaObjetivo() {
@@ -24,14 +28,42 @@ public class ArmaDirigida extends ArmaLimitada {
 	
 	// Cambia la flota objetivo a la que va a disparar el arma.
 	public void setFlotaObjetivo(Flota nuevaFlotaObjetivo) {
-		
+		this.flotaObjetivo = nuevaFlotaObjetivo;
 	}
 	
 	@Override
 	// "Dispara el arma en la dirección hacia la cual apunta, o hacia alguna nave de
 	// la flota objetivo (si es que queda alguna sin destruir) y agrega el proyectil
 	// al escenario en el cual se encuentra. Devuelve el proyectil disparado.
-	public Proyectil disparar() {
-		return null;
+	public Proyectil disparar() {	
+		
+		Vuelo vuelo;
+		Proyectil proyectilDisparado;
+		NaveMilitar potencialObjetivo;
+		
+		proyectilDisparado = super.disparar();
+		
+		
+		// Ahora tenemos un proyectil sin objetivo. Hay que ver si podemos 
+		// encontrar alguna nave sin destruir en la flota y dirigirle el 
+		// proyectil.
+		
+		if (this.flotaObjetivo != null) {
+			
+			Iterator<NaveMilitar> navesFlotaObjetivoIterator = this.flotaObjetivo.getNaves().iterator();
+			
+			while (navesFlotaObjetivoIterator.hasNext()) {
+				
+				potencialObjetivo = navesFlotaObjetivoIterator.next();
+				
+				if(! potencialObjetivo.estaDestruido()){
+					vuelo = new VueloDirigido(proyectilDisparado, potencialObjetivo);
+					proyectilDisparado.setVuelo(vuelo);
+					return proyectilDisparado;
+				}
+			}
+		}
+		
+		return proyectilDisparado;
 	}
 }
--- tp2-codigo-fuente/tp2/modelo/ArmaLimitada.java
+++ tp2-codigo-fuente/tp2/modelo/ArmaLimitada.java
@@ -21,27 +21,32 @@ public class ArmaLimitada extends Arma {
 	@Override
 	// Ordena al arma actuar en el escenario durante el tiempo específicado. Si no  
 	// tiene, se levanta una excepción.
-	public void actuarDurante(double tiempo) {
+	public void actuarDurante(double unTiempo) {
+
+		if (this.getCarga() == (Number) 0)
+			this.frenarDisparos();
 		
+		super.actuarDurante(unTiempo);
 	}
 	
 	// Recibe el número de proyectiles con los cuales se va a cargar el arma, y se 
 	// los suma al número actual.
 	public void cargarCon(int unaCarga) {
-		carga += unaCarga; 
+		this.carga += unaCarga; 
 	}
 	
 	// Recibe el número de proyectiles que se van a descargar del arma, y se los  
 	// resta al número actual.
 	public void descargarEn(int unaCarga) {
-		
+		this.carga -= unaCarga;
 	}
 	
 	// "Dispara el arma en la dirección hacia la cual apunta, y agrega el proyectil
 	// al escenario en el cual se encuentra. La carga disminuye en una unidad.
 	// Se supone que este método protegido no se llama si la carga es cero.
 	public Proyectil disparar() {
-		return null;
+		this.descargarEn(1);
+		return super.disparar();
 	}
 
 	@Override
