44a14769cafa7bed78ebe99450f2f5301a595cee
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-25 20:13:48 +0000

    Actualización de clase Proyectil: solo falta hacer deep cloaning sobre tipos no primitivos (solo realiza clonación simple).

--- tp2-codigo-fuente/tp2/modelo/Proyectil.java
+++ tp2-codigo-fuente/tp2/modelo/Proyectil.java
@@ -1,13 +1,16 @@
 package tp2.modelo;
 
+import java.util.*;
+
 import tp2.auxiliares.Point;
+import tp2.modelo.excepciones.*;
 
 
 // Un proyectil es capaz de dañar naves no aliadas al impactar con ellas en el escenario.
 public class Proyectil extends ObjetoVolador implements Cloneable {
 
 	// El daño por impacto del proyectil.
-	protected double danio;
+	private double danio;
 	
 	
 	// Constructor
@@ -15,7 +18,9 @@ public class Proyectil extends ObjetoVolador implements Cloneable {
 	// velocidad y daño recibidos.
 	public Proyectil(Point posicion, double tamanio, double velocidad, double danio) {
 		super(posicion, tamanio, null, velocidad);
+		
 		this.danio = danio;
+		this.comportamientoAlChocar = new ChoqueDeProyectil(this);
 	}
 	
 	@Override
@@ -29,8 +34,14 @@ public class Proyectil extends ObjetoVolador implements Cloneable {
 	    }
 	    catch(CloneNotSupportedException e) { }
 	    
-	    ((Proyectil)clone).getComportamiento();
-	    
+	    // FALTA CLONAR TIPOS NO PRIMITIVOS!!!!!!!!!!!!!!!!!!!!
+	    /*((Proyectil)clone).getPosicion();
+	    ((Proyectil)clone).setPosicion((Point)posicion.clone());
+	    ((Proyectil)clone).getEscenario();
+	    ((Proyectil)clone).setEscenario((Escenario)escenario.clone());
+	    ((Proyectil)clone).getIdentificacion();
+	    ((Proyectil)clone).setIdentificacion((String)identificacion.clone());*/
+	      
 		return clone;
 	}
 	
@@ -40,6 +51,22 @@ public class Proyectil extends ObjetoVolador implements Cloneable {
 	// vuelo, se levanta una excepción.
 	public void actuarDurante(double unTiempo) {
 		
+		if (this.estaDestruido()) {
+			this.desaparecerDelEscenario();
+			return;
+		}
+	
+		if (unTiempo <= 0) return;
+		
+		Set<ObjetoEspacial> objetosChocados;
+		
+		objetosChocados = (this.getEscenario()).getObjetosEnColisionCon(this);
+		Iterator<ObjetoEspacial> objetosChocadosIterator = objetosChocados.iterator();
+		
+		while (objetosChocadosIterator.hasNext())
+			this.chocarCon(objetosChocadosIterator.next());
+		
+		this.moverDurante(unTiempo);
 	}
 	
 	@Override
@@ -47,6 +74,7 @@ public class Proyectil extends ObjetoVolador implements Cloneable {
 	// comportamiento.
 	public void responderChoqueDe(ObjetoEspacial unObjetoEspacial) {
 		
+		unObjetoEspacial.getComportamiento().sufrirChoqueDeProyectil(this);
 	}
 	
 	public double getDanio() {
@@ -56,7 +84,11 @@ public class Proyectil extends ObjetoVolador implements Cloneable {
 	// Cambia el daño del proyectil a un nuevo valor mayor o igual a cero. Si se 
 	// recibe un daño negativo, entonces se levanta una excepción.
 	public void setDanio(double nuevoDanio) {
+
+		if (nuevoDanio < 0)
+			throw new ValorInvalido("No se puede asignar un daño negativo.");
 		
+		this.danio = nuevoDanio;
 	}
 	
 }
