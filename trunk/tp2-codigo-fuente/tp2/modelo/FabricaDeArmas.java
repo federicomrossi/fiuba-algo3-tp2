6f7fa080f45d2b0a66e63d995542a8e617d25d22
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-26 01:41:51 +0000

    FabricaDeArmas.

--- trunk/tp2-codigo-fuente/tp2/modelo/FabricaDeArmas.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/FabricaDeArmas.java
@@ -17,7 +17,7 @@ public class FabricaDeArmas {
 	// de disparo 5. Devuelve el cañón, apuntando por defecto hacia abajo y 
 	// posicionado en el origen.
 	public Arma crearCanionLaser() {
-		Arma canionLaser = new Arma(new Point(0,0),escenarioActual,100,5,new Point(0,-1),"CanonLaser");
+		Arma canionLaser = new Arma(new Point(0,0),escenarioActual,100,5,new Point(0,-1),"CanionLaser");
 		Proyectil laser = new Proyectil(new Point(0,0),0.1,0,10);
 		canionLaser.setModeloDeProyectil(laser);
 		return canionLaser;
