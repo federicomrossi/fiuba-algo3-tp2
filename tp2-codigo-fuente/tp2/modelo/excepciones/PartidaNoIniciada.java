ece826c51cfada3af8014a7ad6dfdcf45708287a
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-26 02:16:48 +0000

    Excepción PartidaNoIniciada finalizada.

--- tp2-codigo-fuente/tp2/modelo/excepciones/PartidaNoIniciada.java
+++ tp2-codigo-fuente/tp2/modelo/excepciones/PartidaNoIniciada.java
@@ -1,23 +1,23 @@
 package tp2.modelo.excepciones;
 
-public class JuegoNoIniciado extends RuntimeException {
+public class PartidaNoIniciada extends RuntimeException {
 
-	public JuegoNoIniciado() {
+	public PartidaNoIniciada() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public JuegoNoIniciado(String message, Throwable cause) {
+	public PartidaNoIniciada(String message, Throwable cause) {
 		super(message, cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public JuegoNoIniciado(String message) {
+	public PartidaNoIniciada(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public JuegoNoIniciado(Throwable cause) {
+	public PartidaNoIniciada(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
