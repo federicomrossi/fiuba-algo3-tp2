75bcba5b38ff30cfb9588ddc9c3714badc5a7eef
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-05-28 21:22:43 +0000

    Implemento el comportamiento de las puntuaciones (falta la documentación), para que ya haya algo que funcione.

--- tp2-codigo-fuente/tp2/modelo/PuntuacionPorPorcentaje.java
+++ tp2-codigo-fuente/tp2/modelo/PuntuacionPorPorcentaje.java
@@ -3,16 +3,17 @@ package tp2.modelo;
 public class PuntuacionPorPorcentaje implements Puntuacion {
 
 	private Escenario escenario;
+	private double porcentaje;
 	
-	public PuntuacionPorPorcentaje(Escenario escenario) {
+	public PuntuacionPorPorcentaje(Escenario escenario, double porcentaje) {
 		super();
 		this.escenario = escenario;
+		this.porcentaje = porcentaje;
 	}
 
 	@Override
 	public int getPuntuacion() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int)(this.escenario.getPuntuacion() * this.porcentaje);
 	}
 
 }
