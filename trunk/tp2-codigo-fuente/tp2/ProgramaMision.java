10f19d36605fa2ba970fcbc78544893245ce9a74
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-17 02:34:55 +0000

    Algunas pruebas de aviones y extras sobre la pantalla.

--- trunk/tp2-codigo-fuente/tp2/ProgramaMision.java
+++ trunk/tp2-codigo-fuente/tp2/ProgramaMision.java
@@ -1,8 +1,16 @@
 package tp2;
 
+import java.util.Iterator;
+import java.util.List;
+
 import tp2.auxiliares.Point;
 import tp2.control.ControladorJuegoAlgo42;
 import tp2.modelo.*;
+import tp2.modelo.extras.Agua;
+import tp2.modelo.extras.Nube;
+import tp2.vista.modelo.extras.VistaAgua;
+import tp2.vista.modelo.extras.VistaNubeTipo1;
+import tp2.vista.modelo.extras.VistaNubeTipo2;
 import tp2.vista.ventanas.VentanaPruebaMision;
 import ar.uba.fi.algo3.titiritero.vista.Ventana;
 
@@ -17,23 +25,49 @@ public class ProgramaMision {
 		controlador.setSuperficieDeDibujo(ventana);
 		ventana.setVisible(true);
 		
+		VistaAgua vistaAgua = new VistaAgua(new Agua());
+		controlador.agregarDibujable(vistaAgua);
+				
 		// Al llegar al menú de misión, dicho menú se encargará de crear la misión (de acuerdo al nivel de la partida que estará en su menú padre de partida), lo cual va a generar las naves como hacemos en este programa de prueba.
 		FabricaDeNaves fabrica = new FabricaDeNaves(controlador.getEscenario(), "equipo1", null);
 		
-		NaveMilitarControlada algo42 = fabrica.crearAlgo42En(new Point(25, 10));
+		NaveMilitarControlada algo42 = fabrica.crearAlgo42En(new Point(45, 10));
 		
 		fabrica.setEquipoDeLaNave("equipo2");
 		
-		Nave explorador = fabrica.crearExploradorEn(new Point(10, 45));
-		Nave explorador2 = fabrica.crearExploradorEn(new Point(-10, 15));
+//		Nave explorador = fabrica.crearExploradorEn(new Point(-30, 45));
+//		Nave explorador2 = fabrica.crearExploradorEn(new Point(-40, 15));
+//		Nave avioneta = fabrica.crearBombarderoEn(new Point(10, 50));
+		List<NaveEnemiga> grupoCaza = fabrica.crearGrupoCazaEn(new Point(30,50));
 		
 		algo42.iniciarFuego();
 		
 		controlador.getEscenario().agregarObjeto(algo42);
-		controlador.getEscenario().agregarObjeto(explorador);
-		controlador.getEscenario().agregarObjeto(explorador2);
+//		controlador.getEscenario().agregarObjeto(explorador);
+//		controlador.getEscenario().agregarObjeto(explorador2);
+//		controlador.getEscenario().agregarObjeto(avioneta);
 		
-		controlador.setIntervaloSimulacion(20);
+		Iterator<NaveEnemiga> i = grupoCaza.iterator();
+		
+		while (i.hasNext()) {
+			controlador.getEscenario().agregarObjeto(i.next());
+		}
+		
+		
+		VistaNubeTipo1 vistaNube1 = new VistaNubeTipo1();
+		Nube nube1 = new Nube(-10, -100, 600, 5);
+		vistaNube1.setPosicionable(nube1);
+		controlador.agregarDibujable(vistaNube1);
+		controlador.agregarObjetoVivo(nube1);
+		
+		VistaNubeTipo2 vistaNube2 = new VistaNubeTipo2();
+		Nube nube2 = new Nube(300, -300, 700, 6);
+		vistaNube2.setPosicionable(nube2);
+		controlador.agregarDibujable(vistaNube2);
+		controlador.agregarObjetoVivo(nube2);
+		
+		
+		controlador.setIntervaloSimulacion(10);
 		controlador.comenzarJuego();
 	}
 
