0f02cd0065db916b9243771f0f4ce4b90a1443a0
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-18 21:04:17 +0000

    La explosión funcionando (falta agregar los id de las naves en ObjetosExplosivos.java) + Algunas correcciones

---
+++ trunk/tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeBonos.java
@@ -0,0 +1,8 @@
+package tp2.modelo.especificaciones;
+
+public class ValoresDeBonos {
+	
+	public static double bonoDeArmasTamanio = 5;
+	public static double bonoDeEnergiaTamanio = 5;
+	
+}
--- trunk/tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeNaves.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeNaves.java
@@ -7,49 +7,55 @@ package tp2.modelo.especificaciones;
  */
 public class ValoresDeNaves {
 	
-	public static int algo42Tamanio = 7;
-	public static int algo42Velocidad = 25;
-	public static int algo42Energia = 500;
+	public static String algo42Identificacion = "Algo42";
+	public static double algo42Tamanio = 7;
+	public static double algo42Velocidad = 25;
+	public static double algo42Energia = 500;
 	
-	public static int avionCivilTamanio = 5;
-	public static int avionCivilVelocidad = 40;
-	public static int avionCivilEnergia = 1;
+	public static String avionCivilIdentificacion = "AvionCivil";
+	public static double avionCivilTamanio = 5;
+	public static double avionCivilVelocidad = 20;
+	public static double avionCivilEnergia = 1;
 	public static int avionCivilPenalizacion = 300;
 	
-	public static int avionetaTamanio = 3;
-	public static int avionetaVelocidad = 60;
-	public static int avionetaEnergia = 10;
+	public static String avionetaIdentificacion = "Avioneta";
+	public static double avionetaTamanio = 3;
+	public static double avionetaVelocidad = 60;
+	public static double avionetaEnergia = 10;
 	public static int avionetaPuntuacion = 20;
 	
-	public static int bombarderoTamanio = 7;
-	public static int bombarderoVelocidad = 28;
-	public static int bombarderoEnergia = 100;
+	public static String bombarderoIdentificacion = "Bombardero";
+	public static double bombarderoTamanio = 7;
+	public static double bombarderoVelocidad = 28;
+	public static double bombarderoEnergia = 100;
 	public static double bombarderoVueloAmplitud = 10;
 	public static int bombarderoPuntuacion = 30;
-	public static double bombarderoBonoDeArmasTamanio = 5;
 	
-	public static int exploradorTamanio = 5;
-	public static int exploradorVelocidad = 48;
-	public static int exploradorEnergia = 1;
+	public static String exploradorIdentificacion = "Explorador";
+	public static double exploradorTamanio = 5;
+	public static double exploradorVelocidad = 48;
+	public static double exploradorEnergia = 1;
 	public static double exploradorVueloLongitudTrayectoria = 40;
 	public static int exploradorPuntuacion = 50;
 	
-	public static int cazaTamanio = 6;
-	public static int cazaVelocidad = 40;
-	public static int cazaEnergia = 30;
-	public static int cazaUnidadesPorFormacion = 3;
+	public static String cazaIdentificacion = "Caza";
+	public static double cazaTamanio = 6;
+	public static double cazaVelocidad = 40;
+	public static double cazaEnergia = 30;
+	public static double cazaUnidadesPorFormacion = 3;
 	public static int cazaPuntuacion = 30;
-	public static double cazaBonoDeEnergiaTamanio = 5;
 	
-	public static int guiaEnemigoTamanio = 15;
-	public static int guiaEnemigoVelocidad = 20;
-	public static int guiaEnemigoEnergia = 1000;
+	public static String guiaEnemigoIdentificacion = "GuiaEnemigo";
+	public static double guiaEnemigoTamanio = 15;
+	public static double guiaEnemigoVelocidad = 20;
+	public static double guiaEnemigoEnergia = 1000;
 	public static double guiaEnemigoVueloLineaRectaLongitudTrayectoria = 50;
 	public static double guiaEnemigoVueloCirculosLongitudTrayectoria = 1;
 	
-	public static int helicopteroFederalTamanio = 5;
-	public static int helicopteroFederalVelocidad = 20;
-	public static int helicopteroFederalEnergia = 1;
+	public static String helicopteroFederalIdentificacion = "HelicopteroFederal";
+	public static double helicopteroFederalTamanio = 5;
+	public static double helicopteroFederalVelocidad = 20;
+	public static double helicopteroFederalEnergia = 1;
 	public static double helicopteroFederalVueloLongitudTrayectoria = 30;
 	public static int helicopteroFederalPenalizacion = 200;
 }
