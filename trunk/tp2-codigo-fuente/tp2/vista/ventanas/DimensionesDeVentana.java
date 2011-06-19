b49e0952a92f615d4adaf63a49e747fe246959ec
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-19 13:52:49 +0000

    Dimensión de la ventana, ajustada.

--- trunk/tp2-codigo-fuente/tp2/vista/ventanas/DimensionesDeVentana.java
+++ trunk/tp2-codigo-fuente/tp2/vista/ventanas/DimensionesDeVentana.java
@@ -2,8 +2,8 @@ package tp2.vista.ventanas;
 
 public class DimensionesDeVentana {
 
-	public static int alto = 600;
-	public static int ancho = 600;
+	public static int alto = 500;
+	public static int ancho = 500;
 	public static int altoBarraDeVentana = 30;
 
 	public static int centroX = 250;
