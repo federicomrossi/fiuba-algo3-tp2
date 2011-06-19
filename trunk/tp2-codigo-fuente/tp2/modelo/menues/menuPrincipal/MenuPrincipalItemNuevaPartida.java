68f8ee7e801d3c5c26ad63d618869c272b1b51d1
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-19 19:45:41 +0000

    subo un toque de hardcodeo de prueba.

--- trunk/tp2-codigo-fuente/tp2/modelo/menues/menuPrincipal/MenuPrincipalItemNuevaPartida.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/menues/menuPrincipal/MenuPrincipalItemNuevaPartida.java
@@ -1,6 +1,16 @@
 package tp2.modelo.menues.menuPrincipal;
 
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import ar.uba.fi.algo3.titiritero.vista.Imagen;
+import tp2.auxiliares.ParCadenaPosicion;
+import tp2.auxiliares.Point;
+import tp2.modelo.Escenario;
+import tp2.modelo.Mision;
 import tp2.modelo.menues.MenuI;
 import tp2.modelo.menues.MenuItem;
 import tp2.modelo.menues.menuNuevaPartida.MenuNuevaPartida;
@@ -30,6 +40,20 @@ public class MenuPrincipalItemNuevaPartida extends MenuItem {
 	@Override
 	public void seleccionar() {
 		MenuNuevaPartida menuNuevaPartida = new MenuNuevaPartida(this.getVentanaPrincipal(), this.getMenuDelItem());
-		menuNuevaPartida.mostrar();
+		
+		Escenario escenario = new Escenario(new Rectangle(140, 140));
+		Mision mision = new Mision(escenario);
+		
+		Map<Double, Collection<ParCadenaPosicion>> datos = new HashMap<Double, Collection<ParCadenaPosicion>>();
+		ParCadenaPosicion par = new ParCadenaPosicion("asdf Falta hacer parser", new Point(40, 100));
+		for(int i = 0; i < 30; i++){
+			Collection<ParCadenaPosicion> lista = new ArrayList<ParCadenaPosicion>();
+			lista.add(par);
+			datos.put((double)i, lista);
+		}
+		mision.generar(datos);
+		this.getVentanaPrincipal().getControladorJuego().setMision(mision);
+		
+//		menuNuevaPartida.mostrar();
 	}
 }
