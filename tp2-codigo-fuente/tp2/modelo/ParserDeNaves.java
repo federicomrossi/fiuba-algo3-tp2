9a9974508449c0954b2bd711fd4995e5b71d9feb
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-20 05:12:59 +0000

    Pequeño cambio en Mision, para que el parser pueda cargar grupos de naves (como los cazas).

--- tp2-codigo-fuente/tp2/modelo/ParserDeNaves.java
+++ tp2-codigo-fuente/tp2/modelo/ParserDeNaves.java
@@ -1,14 +1,21 @@
 package tp2.modelo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import tp2.auxiliares.ParCadenaPosicion;
 
 public class ParserDeNaves {
 
-	public static Nave fabricarNave(ParCadenaPosicion datoDeLaNave,
-			FabricaDeNaves fabrica, Flota flota) {
+	public static Collection<Nave> fabricarNave(ParCadenaPosicion datoDeLaNave,
+			FabricaDeNaves fabrica, Flota flotaMilitar) {
+		Collection<Nave> naves = new ArrayList<Nave>();
+		
 		NaveEnemiga nave = fabrica.crearExploradorEn(datoDeLaNave.getPosicion());
-		flota.agregarNave(nave);
-		return nave;
+		flotaMilitar.agregarNave(nave);
+		naves.add(nave);
+		
+		return naves;
 	}
 
 }
