cc8651d5a28f3c03e76e4579026b0b85d11a780b
federicomrossi@gmail.com <federicomrossi@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-23 02:06:37 +0000

    Hecha la pausa con la tecla P en el juego y creada la pantalla de game over para ser utilizada al finalizar una partida o perder una misión.

---
+++ trunk/tp2-codigo-fuente/tp2/vista/menues/menuMision/ImagenGameOver.java
@@ -0,0 +1,27 @@
+package tp2.vista.menues.menuMision;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class ImagenGameOver extends Imagen {
+	
+	public static ImagenGameOver imagenPrincipal; 
+
+	private ImagenGameOver() {
+		this.setNombreArchivoImagen("/tp2/vista/imagenes/extras/juego-finalizado.png");
+	}
+
+	private ImagenGameOver(Imagen imagen) {
+		super(imagen);
+	}
+	
+	private static void crearImagenGameOver(){
+		imagenPrincipal = new ImagenGameOver();
+	}
+	
+	public static ImagenGameOver nuevaImagen(){
+		if(imagenPrincipal == null){
+			crearImagenGameOver();
+		}
+		return new ImagenGameOver(imagenPrincipal);
+	}
+}
