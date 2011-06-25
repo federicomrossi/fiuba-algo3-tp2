8a92723425bb1a54fc899970621233ac847ae3ef
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-25 21:01:49 +0000

    Cambiada velocidad del avión civil.

--- tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeNaves.java
+++ tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeNaves.java
@@ -14,7 +14,7 @@ public class ValoresDeNaves {
 	
 	public static String avionCivilIdentificacion = "AvionCivil";
 	public static double avionCivilTamanio = 5;
-	public static double avionCivilVelocidad = 20;
+	public static double avionCivilVelocidad = 45;
 	public static double avionCivilEnergia = 1;
 	public static int avionCivilPenalizacion = 300;
 	
@@ -55,7 +55,7 @@ public class ValoresDeNaves {
 	
 	public static String helicopteroFederalIdentificacion = "HelicopteroFederal";
 	public static double helicopteroFederalTamanio = 5;
-	public static double helicopteroFederalVelocidad = 20;
+	public static double helicopteroFederalVelocidad = 30;
 	public static double helicopteroFederalEnergia = 1;
 	public static double helicopteroFederalVueloLongitudTrayectoria = 60;
 	public static int helicopteroFederalPenalizacion = 200;
