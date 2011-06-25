7b7fda302c0208cd597068fb7b11ec688236a4a0
lucasawad@gmail.com <lucasawad@gmail.com@2469fd7d-57f4-aa94-bae0-26d01c181314>
2011-06-25 18:31:25 +0000

    Pongo el valor de ganar de misión de vuelta en 1000 (perdón pero el enunciado del TP dice eso :( ), y también arreglo el bug de duplicación de armas que saltó (malditas comparaciones de String).

--- trunk/tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeLaPartida.java
+++ trunk/tp2-codigo-fuente/tp2/modelo/especificaciones/ValoresDeLaPartida.java
@@ -2,7 +2,7 @@ package tp2.modelo.especificaciones;
 
 public class ValoresDeLaPartida {
 
-	public static final int puntuacionDeTriunfoDeMision = 500;
+	public static final int puntuacionDeTriunfoDeMision = 1000;
 	public static final int cantidadDeNiveles = 2;
 	public static final int vidasIniciales = 5;
 
